4f8b7ce9cff148fd6f251b6dcda22437219600f3
Kim, Sunho <63573287+kimsunho940904@users.noreply.github.com>
2022-02-19 22:16:41 +0900

    프록시 == , instance of 차이

--- ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
+++ ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
@@ -15,15 +15,24 @@ public class JpaMain {
         tx.begin();
 
         try {
-            Member member = new Member();
-            member.setUsername("memberA");
-            em.persist(member);
+            Member member1 = new Member();
+            member1.setUsername("member1");
+            em.persist(member1);
+
+            Member member2 = new Member();
+            member2.setUsername("member2");
+            em.persist(member2);
 
             em.flush();
             em.clear();
 
-            Member findMember = em.find(Member.class, member.getId());
-            System.out.println("findMember.getUsername() = " + findMember.getUsername());
+            Member m1 = em.find(Member.class, member1.getId());
+//            Member m2 = em.find(Member.class, member2.getId());
+            Member m2 = em.getReference(Member.class,member2.getId());
+            // true
+//            System.out.println("m1 == m2" + (m1.getClass() == m2.getClass()));
+
+            logic(m1,m2);
 
             tx.commit();
         } catch (Exception e) {
@@ -32,5 +41,10 @@ public class JpaMain {
             em.close();
         }
         emf.close();
+    }
+
+    private static void logic(Member m1, Member m2) {
+        System.out.println("m1 == m2" + (m1 instanceof Member));
+        System.out.println("m1 == m2" + (m2 instanceof Member));
     }
 }
